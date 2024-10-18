7fb8e0e81bde67c2d0e4eeddb686163c54473f1f
arcangelo7 <arcangelomas@gmail.com>
2024-10-18 17:27:11 +0200

    ssh on localhost

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 from heritrace.app import app
 
 if __name__ == '__main__':
-    app.run(debug=True, host='localhost', port=5001)
+    app.run(host='127.0.0.1', port=5001, ssl_context=('cert.pem', 'key.pem'), debug=True)
